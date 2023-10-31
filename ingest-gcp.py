2c80907766f39a7bf0fa7d6658563ae7036e06d3
Son Sunt <47473184+sonsunt@users.noreply.github.com>
2023-10-31 17:25:04 +0700

    change load table name to be more meaningful

--- ingest-gcp.py
+++ ingest-gcp.py
@@ -101,7 +101,7 @@ def load_to_bigquery(csv_uri, table_id, cred):
 def main():
     cfg = load_config("config.json")
     bucket_name = "de-general-bucket-1"
-    table_id = "de-zoomcamp-400010.coin_price_data.test_data"
+    table_id = "de-zoomcamp-400010.coin_price_data.latest_prices"
     file_name = f"crypto-prices-{datetime.now():%Y-%m-%d-%H%M%S}"
     json_fp = f"lake-coin/data/json/{file_name}.json"
     csv_fp = f"lake-coin/data/csv/{file_name}.csv"
